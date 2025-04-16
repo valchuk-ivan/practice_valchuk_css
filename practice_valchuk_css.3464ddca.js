1686d5897015902d860747b225cacfc95c33f38b
Ivan Valchuk <i.s.valchuk@gmail.com>
2025-04-17 02:09:51 +0300

    Updates

--- practice_valchuk_css.3464ddca.js
+++ practice_valchuk_css.3464ddca.js
@@ -160,11 +160,11 @@
       });
     }
   }
-})({"6gbjq":[function(require,module,exports,__globalThis) {
+})({"1Ja3l":[function(require,module,exports,__globalThis) {
 var global = arguments[3];
 var HMR_HOST = null;
 var HMR_PORT = null;
-var HMR_SERVER_PORT = 1234;
+var HMR_SERVER_PORT = 50607;
 var HMR_SECURE = false;
 var HMR_ENV_HASH = "d6ea1d42532a7575";
 var HMR_USE_SSE = false;
@@ -668,6 +668,6 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 
 },{}],"j4kuM":[function(require,module,exports,__globalThis) {
 
-},{}]},["6gbjq","j4kuM"], "j4kuM", "parcelRequire94c2", {})
+},{}]},["1Ja3l","j4kuM"], "j4kuM", "parcelRequire94c2", {})
 
 //# sourceMappingURL=practice_valchuk_css.3464ddca.js.map
